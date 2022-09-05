079aea3acaf6823e038f86a6588d6722f8f5dc5b
USER <USER@JIRAPATPC2019>
2022-09-05 11:44:37 +0700

    Add project files.

---
+++ Project1/Game1.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrayNotify;
+
+namespace Project1
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Texture2D bGtile;
+        Texture2D farmer;
+        Vector2 pos;
+        int speed = 3;
+        int direction = 0;
+
+        int frame;
+        int totalframe;
+        int framepersec;
+        float timeperframe;
+        float totalelapsed;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            bGtile = base.Content.Load<Texture2D>("IMG_0130");
+            farmer = Content.Load<Texture2D>("Char01");
+            frame = 0;
+            totalframe = 4;
+            framepersec = 12;
+            timeperframe = (float)1 / framepersec;
+            totalelapsed = 0;
+            pos = new Vector2(20,270);
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            ProcessInput();
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            float elapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            KeyboardState ks = Keyboard.GetState();
+            {
+                if (ks.IsKeyDown(Keys.W))
+                {
+                    pos.Y = 270;
+                    direction = 3;
+                    UpdateFrame((float)gameTime.ElapsedGameTime.TotalSeconds);
+                }
+                if (ks.IsKeyDown(Keys.S))
+                {
+                    pos.Y = 350;
+                    direction = 0;
+                    UpdateFrame((float)gameTime.ElapsedGameTime.TotalSeconds);
+                }
+                if (ks.IsKeyDown(Keys.A))
+                {
+                    pos.X = pos.X - speed;
+                    direction = 1;
+                    UpdateFrame((float)gameTime.ElapsedGameTime.TotalSeconds);
+                }
+                if (ks.IsKeyDown(Keys.D))
+                {
+                    pos.X = pos.X + speed;
+                    direction = 2;
+                    UpdateFrame((float)gameTime.ElapsedGameTime.TotalSeconds);
+                }
+            }
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(bGtile, new Vector2(0f, 0f), new Rectangle?(new Rectangle(0, 0, bGtile.Width, bGtile.Height)), Color.White);
+            _spriteBatch.Draw(farmer, pos, new Rectangle(32 * frame, 48 * direction, 32, 48), (Color.White));
+            // TODO: Add your drawing code here
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+        void UpdateFrame(float elapsed)
+        {
+            totalelapsed += elapsed;
+            if (totalelapsed > timeperframe)
+            {
+                frame = (frame + 1) % totalframe;
+                totalelapsed -= timeperframe;
+            }
+        }
+        protected void ProcessInput()
+        {
+            // TODO: Add your input handle here
+
+
+        }
+    }
+}
